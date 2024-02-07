680f04ad321431e8ef54af28f7d7ac1b1c89e4ac
Iceeeeup <119989243+Iceeeeup@users.noreply.github.com>
2024-02-07 19:32:13 +0700

    1

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import TBM from './popup/login.js';
+
+
+
+
+
+function App() {
+  
+  
+  
+  return (
+    <div >
+      <TBM/>
+      
+    
+    
+  </div>
+    
+    
+  );
+}
+
+export default App;
---
+++ src/popup/login.js
@@ -0,0 +1,35 @@
+import React from "react";
+import './login.css';
+
+
+function TBM(){
+    
+  
+    
+    
+    return(
+        <>
+        <section>
+            <div class="tbm">
+           <h class="talk">Talk about me</h>
+           
+           <div class="log">
+            <h class="login">Login</h>
+            </div>
+            </div>
+            
+    
+        </section>
+        </>
+        
+
+        
+        
+    );
+    
+}
+
+
+
+
+export default TBM;
